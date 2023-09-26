a029c1a21db998880e11d40518779ff966d4987c
Brandon <brandonaguerodeveloper@gmail.com>
2023-09-26 13:18:09 -0500

    Validaciones con javascript, agregamos un estilos para el mensaje en caso de que se encuentre un error con el email

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", () => {
+  const sectionForm = document.querySelector(".section__form");
+  const valueInput = document.querySelector(".form__input--text");
+  const message = document.querySelector(".form__message-errors");
+
+  const symbolsNotAllowed = "&$#*%?!*'\" ";
+
+  sectionForm.addEventListener("submit", (e) => {
+    message.style.color = "#fb3e3e";
+    e.preventDefault();
+
+    const lengthEmail = valueInput.value.length;
+    const isEmail = valueInput.value.endsWith("@gmail.com");
+
+    const emailArray = valueInput.value.split("");
+    const index = emailArray.at(-10);
+    const result = emailArray.splice(0, emailArray.lastIndexOf(index));
+
+    if (lengthEmail === 0) {
+      message.textContent = "Oops! Please add your email";
+      return;
+    }
+
+    if (lengthEmail <= 10) {
+      message.textContent = "Oops! Please add a complete email address";
+      return;
+    }
+
+    const isContainsSymbol = result.some((symbol) => symbolsNotAllowed.includes(symbol));
+    if (isContainsSymbol) {
+      message.textContent = "Symbols not allowed";
+      return;
+    }
+
+    if (!isEmail) {
+      message.textContent = "Oops! Please check your email";
+      return;
+    }
+
+    if (result.includes(index)) {
+      message.textContent = "Oops! Two or more '@' symbols are not allowed";
+      return;
+    }
+
+    message.textContent = "Access Granted";
+    message.style.color = "#00FF00";
+  });
+});
